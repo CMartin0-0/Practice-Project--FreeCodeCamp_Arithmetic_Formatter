baa5aca28943ab2d2f85783c12ab283d1bd4b2ec
Caleb Martin <calebmartin2020@gmail.com>
2025-01-22 19:59:06 +0000

    more progress on arranger function

--- arithmetic-arranger.py
+++ arithmetic-arranger.py
@@ -8,23 +8,46 @@ def arithmetic_arranger(problems, show_answers=False):
     line3 = ""
     line4 = ""
     
+    #Check to see if there are too many problems    
     try:
         if len(problems) > 5:
             raise BaseException
     except:
         return "Error: Too many problems."
     
+    #Separate and store each problem as separate parts and check exceptions
     for problem in problems:
-        operator = re.search('\\s[+-]\\s', problem)
-        if operator is None: 
+        operator = re.search('[+-]', problem)
+        
+        try:
+            if operator is None:
+                raise BaseException
+        except:     
             return "Error: Operator must be '+' or '-'."
+        
         operator = operator.group().strip()
-   
-        print(operator)
+        operands = problem.split(operator)
+        x = operands[0].strip()
+        y = operands[1].strip()
+        
+        try:
+            if not x.isdigit() or not y.isdigit():
+                raise BaseException
+        except:
+            return "Error: Numbers must only contain digits." 
+        try:
+            if len(x) > 4 or len(y) > 4:
+                raise BaseException
+        except:
+            return "Error: Numbers cannot be more than four digits."
+        
+        
+        
+        print(x,y)
    
     
 
-arithmetic_arranger(["351 + 21"])
+arithmetic_arranger(["35144 + 21", "251 - 15"])
 
         
         
