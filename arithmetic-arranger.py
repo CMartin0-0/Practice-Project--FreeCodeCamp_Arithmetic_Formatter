7a54a7a82c39f0b74fc761ff4de20c0c0881534f
Caleb Martin <calebmartin2020@gmail.com>
2025-01-22 20:46:50 +0000

    completed arranger function

--- arithmetic-arranger.py
+++ arithmetic-arranger.py
@@ -41,13 +41,43 @@ def arithmetic_arranger(problems, show_answers=False):
         except:
             return "Error: Numbers cannot be more than four digits."
         
+        spacing = len(x) + 2 if len(x) > len(y) else len(y) + 2
+        
+        #check to see if a problem has already been arranged
+        if len(line1) > 0:
+            #add in proper spacing between arranged problems
+            line1 += 4 * " " 
+            line2 += 4 * " "
+            line3 += 4 * " "
+        line1 += x.rjust(spacing, " ")
+        line2 += operator + (spacing - 1 - len(y)) * " " + y
+        line3 += spacing * "-"
+        if show_answers == True:
+            if len(line4) > 0:
+                line4 += 4 * " "
+            if operator == "+":
+                solution = int(x) + int(y)
+            else:
+                solution = int(x) - int(y)
+            line4 += str(solution).rjust(spacing, " ")
+    #include solutions in return if show answers is true
+    if show_answers == True:
+        return line1 + "\n" + line2 + "\n" + line3 + "\n" + line4
+    return line1 + "\n" + line2 + "\n" + line3
+                
+            
+    
+            
+        
+            
         
         
-        print(x,y)
+
    
     
+print(arithmetic_arranger(["351 + 21", "251 - 15", "444 + 4", "2 - 333"]))
 
-arithmetic_arranger(["35144 + 21", "251 - 15"])
+
 
         
         
